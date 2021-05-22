2a4ed37d0bb68712a66843405080c1c2df0263ea
muk-tar <72618952+muk-tar@users.noreply.github.com>
2021-05-22 16:49:24 +0530

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -25,7 +25,7 @@ export default function Home() {
                     <NavLink to="/explore"><button className="btn btn-primary">Explore</button></NavLink>
                 
                     <h6>a build of different companies, businesses and entities operating in varied sectors and segments as a collective and complementary aggregate.</h6>
-                    <h6>Airport | Fintech | Industry 4.0 | Logistics |Iot and AI | UTM | Smart City !</h6>
+                    <h6>Airport | Fintech | Industry 4.0 | Logistics |IoT and AI | UTM | Smart City !</h6>
                     <h6>Pioneering businesses and technologies, creating sustainable value with collaborative and indulgent association banking on skill, knowledge, perseverance and steadfastness.</h6>
                 
                 </div>
