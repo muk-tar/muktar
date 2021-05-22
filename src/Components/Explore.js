8cfbd0902b37a28ffa41fe95622aba5dbeb22922
muk-tar <72618952+muk-tar@users.noreply.github.com>
2021-05-22 17:14:20 +0530

    Update Explore.js

--- src/Components/Explore.js
+++ src/Components/Explore.js
@@ -22,16 +22,16 @@ export default function Explore() {
             <NavBar too="/" />
            <div className="explore-container">
                 <Card14 title="STEEL" description="Community Building" title2="Mild Steel" transfer="steel" photo={photo1} />
-                <Card14 title="AEROSPACE" description="The sky is the limit" title2="Design to end-of-life" transfer="aerospace" photo={photo2}/>
-                <Card14 title="NETWORK" description="Network's the computer" title2="communication to computing" transfer="network"  photo={photo3}/>
+                <Card14 title="AEROSPACE" description="The sky is not the limit" title2="Design to end-of-life" transfer="aerospace" photo={photo2}/>
+                <Card14 title="NETWORK" description="Network's the computer" title2="Communication to Computing" transfer="network"  photo={photo3}/>
                 <Card14 title="INFRASTRUCTURE" description="A mile of Runway will take you anywhere" title2="Industrial Park to Smart City" transfer="infrastructure" photo={photo4}/>
                 <Card14 title="AVIATION" description="Into the blue yonder" title2="Airlines to Flying Clubs" transfer="aviation" photo={photo5}/>
-                <Card14 title="SKILLING" description="Knowledge isthe greatest treasure" title2="Centers of Excellence" transfer="skilling" photo={photo6} />
+                <Card14 title="SKILLING" description="Knowledge is the greatest treasure" title2="Centers of Excellence" transfer="skilling" photo={photo6} />
                 <Card14 title="MEDIA" description="The strength of the community" title2="English periodical - aeronautics & general aviation." transfer="explore" photo={photo7} />
                 <Card14 title="EVENTS & RECREATION" description="Work too can be fun" title2="Air Shows to Thematic Tours" transfer="events" photo={photo8} />
                 <Card14 title="AFFLIATES" description="" title2="Bussiness associates" photo={photo9} transfer="affiliates" /> 
-                <Card14 title="About us" title2="About us" photo={photo11}  transfer="aboutus" /> 
-                <Card14  title2="mailto:m.bluth@example.com" photo={photo10} transfer="mailto:m.bluth@example.com" contact="contact" /> 
+                <Card14 title="WE" title2="About us" photo={photo11}  transfer="About Us" /> 
+                <Card14  title2="mailto:rsvp@muk-tar.com" photo={photo10} transfer="mailto:rsvp@muk-tar.com" contact="contact" /> 
             </div>
        </section>
     )
