b8cbb197d916fd6b117813fc734572c505ef4b8f
muk-tar <72618952+muk-tar@users.noreply.github.com>
2021-05-22 17:04:19 +0530

    Update Network.js

--- src/Components/Network.js
+++ src/Components/Network.js
@@ -24,42 +24,42 @@ export default function Network() {
                 <Card6 
                 title="UAS Unmanned aerial systems"
                 description="includes, besides the UAV, elements such as mission planning, ground control stations, data links, launch and recovery, payload, controller and other support equipment"
-                photo={photo1}
+                photo={photo7}
                 alt="img1"
                 />
 
                 <Card6 
                 title="UTM UAV traffic management"
                 description="an air traffic management ecosystem in an urban environment beyond visual line-of-sight at altitudes under 1,000 ft above ground level for autonomously controlled UAS."
-                photo={photo2}
+                photo={photo8}
                 alt="img1"
                 />
 
                 <Card6 
                 title="RVT remote virtual tower"
                 description="a digital air traffic service (ATS) for airports somewhere other than at the airport, managing multiple airports from a single location as contingency and primary services"
-                photo={photo3}
+                photo={photo9}
                 alt="img1"
                 />
 
                 <Card6 
                 title="FIMS Flight information management system"
                 description="critical for all flight management of drones, UAVs, airplanes and other flying objects ensuring incident free movement of aircraft."
-                photo={photo4}
+                photo={photo1}
                 alt="img1"
                 />
 
                 <Card6 
                 title="RF radios and antennae"
                 description="radio frequency manipulation for communication, heating, scanning, measurements and bespoke applications."
-                photo={photo5}
+                photo={photo6}
                 alt="img1"
                 />
 
                 <Card6 
                 title="mATM Inclusive banking solutions"
                 description="banking business correspondent network providing instant money transactions including cash deposits, withdrawals..."
-                photo={photo6}
+                photo={photo5}
                 alt="img1"
                 />
 
@@ -67,21 +67,21 @@ export default function Network() {
                 <Card6 
                 title="Data Center Industry 4.0 and Logistics 4.0 capable"
                 description="Facility equipped to support cloud based infrastructure and applications for automation of industries and supply chain management with interconnection, data transparency and decentralisation"
-                photo={photo7}
+                photo={photo3}
                 alt="img1"
                 />
 
                 <Card6 
                 title="IoT & AI Internet of things and artificial intelligence"
                 description="“things” with sensors, software and data analytics embedded and networked to deliver utility and daily use services in your home, office, factory, warehouse, shop, campus, locality or city"
-                photo={photo8}
+                photo={photo4}
                 alt="img1"
                 />
 
                 <Card6 
                 title="CLEC Cloud and communication solutions"
                 description="Hosted IP-PBX, cloud voice, DLEC, MVNE, MVNA, Video telephony, hosted cloud communications, value added services, soft switching,robotic business process automation, VoWiFi, VoLTE,"
-                photo={photo9}
+                photo={photo2}
                 alt="img1"
                 />
 
